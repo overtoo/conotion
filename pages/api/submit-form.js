281f5b814235581fca508c5038eeb10f4dd15479
overtoo <s_bajo@hotmail.com>
2021-12-10 18:56:13 +0800

    added tags

--- pages/api/submit-form.js
+++ pages/api/submit-form.js
@@ -1,3 +1,23 @@
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
+
 const { Client } = require("@notionhq/client");
 
 const notion = new Client({
@@ -58,3 +78,5 @@ export default async function handler(req, res) {
     res.status(500).json({ msg: "There was an error" });
   }
 }
+
+module.exports = allowCors(handler);
